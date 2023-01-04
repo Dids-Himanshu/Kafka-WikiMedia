bffcbb2b83532c63f45e19fc026ca24252ab2e6d
Himanshu <himanshu@Himanshus-MacBook-Air.local>
2023-01-04 22:57:21 +0530

    first commit

---
+++ kafka-consumer-database/src/main/java/com/kafka/KafkaDatabaseConsumer.java
@@ -0,0 +1,33 @@
+package com.kafka;
+
+import com.kafka.entity.WikimediaData;
+import com.kafka.repository.WikimediaDataRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaDatabaseConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+
+    private WikimediaDataRepository wikimediaDataRepository;
+
+
+    public KafkaDatabaseConsumer(WikimediaDataRepository wikimediaDataRepository) {
+        this.wikimediaDataRepository = wikimediaDataRepository;
+    }
+
+    @KafkaListener(
+            topics = "wikimedia_recentchange",
+            groupId = "myGroup"
+    )
+    private void consume(String eventMessage){
+
+        logger.info(String.format("Event message Received -> %s",eventMessage));
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+        wikimediaDataRepository.save(wikimediaData);
+    }
+}
